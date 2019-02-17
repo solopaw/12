1f7aba23b9bfa23354d28ec8f61a2bd55ece08ae
solopaw <41173209+solopaw@users.noreply.github.com>
2019-02-17 11:37:49 +1000

    Update Leaderboard.lua

--- Leaderboard.lua
+++ Leaderboard.lua
@@ -1,24 +1,26 @@
-PointData = game:GetService("DataStoreService"):GetDataStore("PointData")
-RebirthData = game:GetService("DataStoreService"):GetDataStore("RebirthData")
+local datastore = game:GetService("DataStoreService")
+local ds1 = datastore:GetDataStore("BuxSaveSystem")
+local ds2 = datastore:GetDataStore("PointsSaveSystem")
 
-game.Players.PlayerAdded:Connect(function(plr)
-	local LB = Instance.new("IntValue")
-	LB.Name = "leaderstats"
-	
-	local p = Instance.new("IntValue")
-	p.Name = "Points"
-	p.Value = PointData:GetAsync(plr.userId) or 0
-	
-	local r = Instance.new("IntValue")
-	r.Name = "Rebirths"
-	r.Value = RebirthData:GetAsync(plr.userId) or 0
-
-	LB.Parent = plr
-	p.Parent = LB
-	r.Parent = LB
-end)
-
-game.Players.PlayerRemoving:Connect(function(plr)
-	PointData:SetAsync(plr.userId, plr.leaderstats.Points.Value)
-	RebirthData:SetAsync(plr.userId, plr.leaderstats.Rebirths.Value)
+game.Players.PlayerAdded:connect(function(plr)
+ local folder = Instance.new("Folder", plr)
+ folder.Name = "leaderstats"
+ local B = Instance.new("IntValue", folder)
+ B.Name = "Buxs"
+ local P = Instance.new("IntValue", folder)
+ P.Name = "Points"
+ 
+ B.Value = ds1:GetAsync(plr.UserId) or 0
+ ds1:SetAsync(plr.UserId, gems.Value)
+ 
+ P.Value = ds2:GetAsync(plr.UserId) or 0
+ ds2:SetAsync(plr.UserId, cash.Value)
+ 
+ B.Changed:connect(function()
+  ds1:SetAsync(plr.UserId, gems.Value)
+ end)
+ 
+ P.Changed:connect(function()
+  ds2:SetAsync(plr.UserId, cash.Value)
+ end)
 end)
