39a3c876d596560c3863f5b5e0590c396e52dbe8
solopaw <41173209+solopaw@users.noreply.github.com>
2019-02-17 12:15:22 +1000

    Create System Message.lua

---
+++ System Message.lua
@@ -0,0 +1,11 @@
+Orange = BrickColor.new("Orange")
+Red = BrickColor.Red
+Prefix = "{System}"
+Player = script.Parent
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+	Text = "Welcome to this new server!";
+	Font = Enum.Font.Cartoon;
+	Color = bc.Color;
+	FontSize = Enum.FontSize.Size96;	
+})
+-- Parent = SSS(ServerScriptService)
